464abbc3e2c51f4fa65a63383a87617fdde8f500
Rezzzzzzz <50838221+Rezzzzzzz@users.noreply.github.com>
2019-05-30 16:37:06 +0100

    Add files via upload

--- replicate.py
+++ replicate.py
@@ -1,11 +1,15 @@
+"""Imports necessários"""
+
 import sys
 import os.path
 import time
 import getopt
 import shutil
+from distutils.dir_util import copy_tree
 
 
 def debug(origin, destiny):
+    """Mostra o que o código faz mas sem alterar nada"""
 
     origin_path = os.path.abspath(origin)  # Path da Origem
     origin_list = os.listdir(origin)  # Lista com os ficheiros da Origem
@@ -17,18 +21,27 @@ def debug(origin, destiny):
         origin_path_join = os.path.join(origin_path, origin_list[i])
         origin_raw_string = r"{}".format(origin_list[i])
 
-        for c in range(0, len(destiny_list)):  # Percorre os ficheiros do destino
-            destiny_path_join = os.path.join(destiny_path, destiny_list[c])
-            destiny_raw_string = r"{}".format(destiny_list[c])
-            destiny_path_join_exist = os.path.join(destiny_path, origin_list[i])
+        for len_destiny_list in range(
+                0, len(destiny_list)):  # Percorre os ficheiros do destino
+            destiny_path_join = os.path.join(destiny_path,
+                                             destiny_list[len_destiny_list])
+            destiny_raw_string = r"{}".format(destiny_list[len_destiny_list])
+            destiny_path_join_exist = os.path.join(destiny_path,
+                                                   origin_list[i])
 
             if destiny_raw_string == origin_raw_string:
 
-                if time.ctime(os.path.getmtime(origin_path_join)) != time.ctime(os.path.getmtime(destiny_path_join)) and os.path.isfile(origin_path_join) and os.path.isfile(destiny_path_join):
-                    print("O ficheiro " + str(origin_list[i]) + " iria ser copiado para o diretório Destino.")
+                if time.ctime(
+                        os.path.getmtime(origin_path_join)) != time.ctime(
+                            os.path.getmtime(destiny_path_join)):
+                    print("O ficheiro ou diretorio " + str(origin_list[i]) +
+                          " iria ser copiado para o diretório Destino.")
 
-                elif time.ctime(os.path.getmtime(origin_path_join)) == time.ctime(os.path.getmtime(destiny_path_join)) and os.path.isfile(origin_path_join) and os.path.isfile(destiny_path_join):
-                    print("O ficheiro " + str(origin_list[i]) + " não iria ser copiado para o diretório Destino.")
+                elif time.ctime(
+                        os.path.getmtime(origin_path_join)) == time.ctime(
+                            os.path.getmtime(destiny_path_join)):
+                    print("O ficheiro ou diretorio " + str(origin_list[i]) +
+                          " não iria ser copiado para o diretório Destino.")
 
             if not os.path.exists(destiny_path_join_exist):
                 exist_list.append(str(origin_list[i]))
@@ -37,38 +50,35 @@ def debug(origin, destiny):
 
     if without_duplicates_debug:
         for file_debug in without_duplicates_debug:
-            print("O ficheiro " + str(file_debug) + " iria ser copiado para o diretório Destino.")
+            print("O ficheiro " + str(file_debug) +
+                  " iria ser copiado para o diretório Destino.")
 
 
-def copy(origin, destiny):
-    #  Copia ficheiros da origem para o destino.
-    for src_dir, dirs, files in os.walk(origin):  # Percorre a Origem / Source Folder
-        dst_dir = src_dir.replace(origin, destiny)  # Replace é usado para obter o Path do destino.
+def copy(src, destiny, src_destiny):
+    """Copia diretórios e ficheiros para o destino"""
 
-        if not os.path.exists(dst_dir):
-            os.mkdir(dst_dir) # Cria o diretório destino se ele não existir
+    if os.path.isdir(src) and not os.path.exists(src_destiny):
+        shutil.copytree(src, src_destiny)
 
-        for file_ in files:
-            src_file = os.path.join(src_dir, file_)  # Faz um join do path da Origem com o do ficheiro
-            dst_file = os.path.join(dst_dir, file_)  # Faz um join do path do Destino com o do ficheiro
-
-            if os.path.exists(dst_file):
-                os.remove(dst_file)  # Vê se o ficheiro existe se existir remove
-
-            shutil.copy2(src_file, dst_dir) # Copia o ficheiro para o destino
+    elif os.path.isfile(src):
+        shutil.copy2(src, destiny)
 
 
 def silence_information(origin):
+    """Mostra os números de ficheiros na origem e ficheiros modificados"""
 
-    copy(origin,destiny)
+    copy_tree(origin, DESTINY)
     dir_name_origin = os.path.abspath(origin)
 
     if os.path.isdir(dir_name_origin):
-        sum_origin = sum([len(files) for r, d, files in os.walk(dir_name_origin)])
-        print("\nNúmero de ficheiros na origem: " + str(sum_origin) + "." + " \nNúmero de ficheiros modificados: " + str(sum_origin) + ".")
+        sum_origin = sum(
+            [len(files) for r, d, files in os.walk(dir_name_origin)])
+        print("\nNúmero de ficheiros na origem: " + str(sum_origin) + "." +
+              " \nNúmero de ficheiros modificados: " + str(sum_origin) + ".")
 
 
 def help():
+    """Comando Help mostra a função de todos os comandos"""
     print("""
           -------- Comandos Disponíveis-------- 
 
@@ -81,8 +91,9 @@ def help():
 
 
 def detailed_information(origin, destiny):
+    """Informação detalhada sobre a Origem e o Destino"""
 
-    copy(origin, destiny)  # Copia os ficheiros da origem para o destino
+    copy_tree(origin, destiny)  # Copia os ficheiros da origem para o destino
     origin_path = os.path.abspath(origin)  # Path da origem
     origin_list = os.listdir(origin)  # Lista com ficheiros da origem
     destiny_path = os.path.abspath(destiny)  # Path do destino
@@ -93,13 +104,14 @@ def detailed_information(origin, destiny):
     print("")
 
     for file_name in origin_list:
-
         join_paths = os.path.join(origin_path, file_name)
 
         print("Nome do ficheiro: %s" % file_name)
         print("Tamanho do ficheiro em bytes: %s" % os.path.getsize(join_paths))
-        print("Última vez que foi modificado: %s" % time.ctime(os.path.getmtime(join_paths)))
-        print("Última vez que foi acessado: %s" % time.ctime(os.path.getatime(join_paths)))
+        print("Última vez que foi modificado: %s" %
+              time.ctime(os.path.getmtime(join_paths)))
+        print("Última vez que foi acessado: %s" %
+              time.ctime(os.path.getatime(join_paths)))
         print("Data de criação: %s" % time.ctime(os.path.getctime(join_paths)))
         print("")
 
@@ -107,13 +119,14 @@ def detailed_information(origin, destiny):
     print("")
 
     for file_name in destiny_list:
-
         join_paths = os.path.join(destiny_path, file_name)
 
         print("Nome do ficheiro: %s" % file_name)
         print("Tamanho do ficheiro: %s" % os.path.getsize(join_paths))
-        print("Última vez que foi modificado: %s" % time.ctime(os.path.getmtime(join_paths)))
-        print("Última vez que foi acessado: %s" % time.ctime(os.path.getatime(join_paths)))
+        print("Última vez que foi modificado: %s" %
+              time.ctime(os.path.getmtime(join_paths)))
+        print("Última vez que foi acessado: %s" %
+              time.ctime(os.path.getatime(join_paths)))
         print("Data de criação: %s" % time.ctime(os.path.getctime(join_paths)))
         print("")
 
@@ -124,87 +137,104 @@ def detailed_information(origin, destiny):
 
 if __name__ == "__main__":
 
-    list_exist_main = []
-    origin = "Origem"
-    destiny = "Destino"
+    LIST_EXIST_MAIN = []
+    ORIGIN = "Origem"
+    DESTINY = "Destino"
 
-    terminal_arguments = sys.argv
-    arguments_list = terminal_arguments[1:]
+    TERMINAL_ARGUMENTS = sys.argv
+    ARGUMENTS_LIST = TERMINAL_ARGUMENTS[1:]
 
     try:
-        arguments, values = getopt.getopt(arguments_list, 'vhds',
+        ARGUMENTS, VALUES = getopt.getopt(ARGUMENTS_LIST, 'vhds',
                                           ["origem=", "destino="])
     except getopt.error as err:
         print("Erro no argumento usado consulte o help: ")
         help()
         sys.exit(2)
 
-    if not arguments_list:
+    if not ARGUMENTS_LIST:
 
-        if not os.path.isdir(origin):
-            print("\nO diretorio " + origin + " não existe no seu computador. :(\n")
+        if not os.path.isdir(ORIGIN):
+            print("\nO diretorio " + ORIGIN +
+                  " não existe no seu computador. :(\n")
             sys.exit(2)
 
-        elif not os.path.isdir(destiny):
+        elif not os.path.isdir(DESTINY):
             try:
-                os.mkdir(destiny)
+                os.mkdir(DESTINY)
 
-            except OSError as e:
-                print("\nO diretorio" + destiny + " não foi criado com sucesso! :(\n")
+            except OSError as error:
+                print("\nO diretorio" + DESTINY +
+                      " não foi criado com sucesso! :(\n")
+                print(error)
                 sys.exit(2)
 
             else:
-                print("\nO diretorio " + destiny + " foi criado com sucesso! :)\n")
+                print("\nO diretorio " + DESTINY +
+                      " foi criado com sucesso! :)\n")
 
-        dir_origin = os.path.abspath(origin)  # Path da origem
-        dir_destiny = os.path.abspath(destiny)  # Path do destino
-        origin_list_main = os.listdir(origin)  # Lista com ficheiros da origem
-        destiny_list_main = os.listdir(destiny)  # Lista com ficheiros do destino
-        list_exist_main = []  # Lista usada para retirar os duplicates
+        DIR_ORIGIN = os.path.abspath(ORIGIN)  # Path da origem
+        DIR_DESTINY = os.path.abspath(DESTINY)  # Path do destino
+        ORIGIN_LIST_MAIN = os.listdir(ORIGIN)  # Lista com ficheiros da origem
+        DESTINY_LIST_MAIN = os.listdir(
+            DESTINY)  # Lista com ficheiros do destino
+        LIST_EXIST_MAIN = []  # Lista usada para retirar os duplicates
 
-        for len_origin in range(0, len(origin_list_main)):
-            origin_path_join_main = os.path.join(dir_origin, origin_list_main[len_origin])
-            origin_raw_string_main = r"{}".format(origin_list_main[len_origin])
+        for len_origin in range(0, len(ORIGIN_LIST_MAIN)):
+            origin_path_join_main = os.path.join(DIR_ORIGIN,
+                                                 ORIGIN_LIST_MAIN[len_origin])
+            origin_raw_string_main = r"{}".format(ORIGIN_LIST_MAIN[len_origin])
 
-            for len_destiny in range(0, len(destiny_list_main)):
-                destiny_path_join_main = os.path.join(dir_destiny, destiny_list_main[len_destiny])
-                destiny_raw_string_main = r"{}".format(destiny_list_main[len_destiny])
-                destiny_path_join_exist_main = os.path.join(dir_destiny, origin_list_main[len_origin])
+            for len_destiny in range(0, len(DESTINY_LIST_MAIN)):
+                destiny_path_join_main = os.path.join(
+                    DIR_DESTINY, DESTINY_LIST_MAIN[len_destiny])
+                destiny_raw_string_main = r"{}".format(
+                    DESTINY_LIST_MAIN[len_destiny])
+                destiny_path_join_exist_main = os.path.join(
+                    DIR_DESTINY, ORIGIN_LIST_MAIN[len_origin])
 
                 if destiny_raw_string_main == origin_raw_string_main:
-                    print("O ficheiro ou diretorio " + str(origin_list_main[len_origin]) + " existe no Destino.")
+                    print("O ficheiro ou diretorio " +
+                          str(ORIGIN_LIST_MAIN[len_origin]) +
+                          " existe no Destino.")
 
-                    if time.ctime(os.path.getmtime(origin_path_join_main)) > time.ctime(os.path.getmtime(destiny_path_join_main)):
-                        list_exist_main.append(str(origin_list_main[len_origin]))
+                    if time.ctime(os.path.getmtime(
+                            origin_path_join_main)) > time.ctime(
+                                os.path.getmtime(destiny_path_join_main)):
+                        LIST_EXIST_MAIN.append(
+                            str(ORIGIN_LIST_MAIN[len_origin]))
 
                 elif not os.path.exists(destiny_path_join_exist_main):
-                    list_exist_main.append(str(origin_list_main[len_origin]))
+                    LIST_EXIST_MAIN.append(str(ORIGIN_LIST_MAIN[len_origin]))
 
-        without_duplicates = list(set(list_exist_main))
+        WITHOUT_DUPLICATES = list(set(LIST_EXIST_MAIN))
 
-        if without_duplicates:
-            for file in without_duplicates:
-                copy(origin, destiny)
+        if WITHOUT_DUPLICATES:
+            for file in WITHOUT_DUPLICATES:
+                src_file = os.path.join(
+                    DIR_ORIGIN,
+                    file)  # Faz um join do path da Origem com o do ficheiro
+                src_file_destiny = os.path.join(DIR_DESTINY, file)
+                copy(src_file, DIR_DESTINY, src_file_destiny)
 
             print("\nCópia concluída com sucesso! :)")
 
-    for argument, value in arguments:
+    for argument, value in ARGUMENTS:
 
         if argument in "-s":
-            silence_information(origin)
+            silence_information(ORIGIN)
 
         elif argument in "-h":
             help()
 
         elif argument in "-d":
-            debug(origin, destiny)
+            debug(ORIGIN, DESTINY)
 
         elif argument in "-v":
-            detailed_information(origin, destiny)
+            detailed_information(ORIGIN, DESTINY)
 
         elif argument in "--origem":
-            origin = value
+            ORIGIN = value
 
         elif argument in "--destino":
-            destiny = value
-
+            DESTINY = value
